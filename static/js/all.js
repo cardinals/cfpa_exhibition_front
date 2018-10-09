3c3052ccf23612ddad48b4d55e3c6c56afcc859e
lixue0919 <442019696@qq.com>
2018-10-09 16:26:26 +0800

    展商登陆默认显示欢迎您

--- static/js/all.js
+++ static/js/all.js
@@ -2,6 +2,9 @@
 $(function () {
     //动态加载main
     var paramUrl = getQueryString("url");
+    if(paramUrl == null){
+        paramUrl="/home";
+    }
     loadDiv(urlRewrite(paramUrl));
 });
 /**header-box by li.xue 20180628 */
@@ -21,6 +24,9 @@ axios.get('/xfxhapi/shiro').then(function (res) {
         permissions.push(res.data.permissions[i]);
     }
     realname = res.data.realName;
+    if(realname == null || realname == ""){
+        realname = "欢迎您！"
+    }
     document.querySelector("#realname").innerHTML = realname;
     if(res.data == null && realname == null && realname == ""){
         window.location.href = "login.html";
