cd86402eecd453cdebf31ae4af65d38cd032ba73
rliu666 <rliu666@sina.cn>
2018-10-15 13:45:33 +0800

    展商弹出页分页

--- static/js/prediction/exhprediction_list.js
+++ static/js/prediction/exhprediction_list.js
@@ -142,8 +142,9 @@ var vue = new Vue({
             };
             axios.post('/zhapi/qyjbxx/doFindZsxxByQyjbxx', params).then(function (res) {
                 // debugger
-                var tableTemp = new Array((this.currentPage_user - 1) * this.pageSize_user);
-                this.tableData_user = tableTemp.concat(res.data.result);
+                // var tableTemp = new Array((this.currentPage_user - 1) * this.pageSize_user);
+                // this.tableData_user = tableTemp.concat(res.data.result);
+                this.tableData_user = res.data.result;
                 this.total_user = res.data.result.length;
                 this.userListVisible = true;
                 this.loading_user = false;
@@ -221,7 +222,7 @@ var vue = new Vue({
                     //     message: ''
                     // });
                 });
-                
+
             }
 
         }
