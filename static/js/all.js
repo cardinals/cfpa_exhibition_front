0c01ff1dfe7854977520b163c99d37f9c07ca7d0
lixue0919 <442019696@qq.com>
2018-10-15 13:22:28 +0800

    session失效后点击页面跳转回登录页

--- static/js/all.js
+++ static/js/all.js
@@ -1,42 +1,48 @@
-//异步加载详情页
-$(function () {
-    //动态加载main
-    var paramUrl = getQueryString("url");
-    if(paramUrl == null){
-        paramUrl="/home";
-    }
-    loadDiv(urlRewrite(paramUrl));
-});
 /**header-box by li.xue 20180628 */
 var shiroGlobal = "";
 var realname = "";
 var permissions = [];
-axios.get('/xfxhapi/shiro').then(function (res) {
-    if(res.data.organizationVO == null || res.data.organizationVO == ""){
-        res.data.organizationVO = {
-            uuid: "",
-            jgjc: ""
+//异步加载详情页
+$(function () {
+    axios.get('/xfxhapi/shiro').then(function (res) {
+        if(res.data.organizationVO == null || res.data.organizationVO == ""){
+            res.data.organizationVO = {
+                uuid: "",
+                jgjc: ""
+            }
         }
-    }
-    shiroGlobal = res.data;
-    //用户权限
-    for(var i in res.data.permissions){
-        permissions.push(res.data.permissions[i]);
-    }
-    realname = res.data.realName;
-    if(realname == null || realname == ""){
-        realname = "欢迎您！"
-    }
-    if(res.data.deptid == "GLYH"){
-        document.getElementById("personal").style.display = "none";
-    }
-    document.querySelector("#realname").innerHTML = realname;
-    if(res.data == null && realname == null && realname == ""){
-        window.location.href = "login.html";
-    }
-}.bind(this), function (error) {
-    console.log(error)
+        shiroGlobal = res.data;
+        if(res.data != null && res.data != ""){
+            //动态加载main
+            var paramUrl = getQueryString("url");
+            if(paramUrl == null){
+                paramUrl="/home";
+            }
+            loadDiv(urlRewrite(paramUrl));
+
+            //用户权限
+            for(var i in res.data.permissions){
+                permissions.push(res.data.permissions[i]);
+            }
+            realname = res.data.realName;
+            if(realname == null || realname == ""){
+                realname = "欢迎您！"
+            }
+            if(res.data.deptid == "GLYH"){
+                document.getElementById("personal").style.display = "none";
+            }
+            document.querySelector("#realname").innerHTML = realname;
+            if(res.data == null && realname == null && realname == ""){
+                window.location.href = "login.html";
+            }
+        }else{
+            window.location.href = "/templates/login.html";
+        }
+    }.bind(this), function (error) {
+        console.log(error)
+    });
 });
+
 
 //axios默认设置cookie
 axios.defaults.withCredentials = true;	
