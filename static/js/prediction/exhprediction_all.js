0904ab518d5bac67c1488da029691d9ad1d6c18a
lixue0919 <442019696@qq.com>
2018-10-17 14:01:47 +0800

    展商登陆成功后，登陆人显示信息

--- static/js/prediction/exhprediction_all.js
+++ static/js/prediction/exhprediction_all.js
@@ -14,6 +14,11 @@ $(function () {
             for(var i in res.data.permissions){
                 permissions.push(res.data.permissions[i]);
             }
+            realname = res.data.username;
+            if(realname == null || realname == ""){
+                realname = "欢迎您！"
+            }
+            document.querySelector("#realname").innerHTML = realname;
         }else{
             window.location.href = "/templates/login.html";
         }
