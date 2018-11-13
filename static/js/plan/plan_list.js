07b75ae253b55e2506965f6ff72cb88cf1f2e5d1
JC2177985 <14690588@qq.com>
2018-11-13 14:21:48 +0800

    展馆平面图模块

---
+++ static/js/plan/plan_list.js
@@ -0,0 +1,31 @@
+//axios默认设置cookie
+axios.defaults.withCredentials = true;
+var viewerHandshake = null
+var vue = new Vue({
+    el: '#app',
+    data: function () {
+        return {
+            currentArea: null,
+            currentAreaStage: null,
+            tableData: [],
+            zguuid: '',
+            ploter: {
+                show: true
+            },
+            dialogVisible: false
+        }
+    },
+    mounted: function () {
+        
+    },
+    computed: {
+        ploterStyle() {
+            return {
+                display: this.ploter.show ? 'flex' : 'none'
+            }
+        }
+    },
+    methods: {
+
+    }
+})
